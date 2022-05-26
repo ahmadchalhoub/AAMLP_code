9f3103fe276c294e48f57665efbddbb3c9de0954
Ahmad <chalhoah@gmail.com>
2022-05-25 20:11:42 -0400

    Complete the first exercise of cross-validation chapter (decision tree testing)

--- cross-validation/cross_val_decision_tree_test.py
+++ cross-validation/cross_val_decision_tree_test.py
@@ -6,11 +6,15 @@
 # There is two different ways that this problem can be looked at: classification and
 # regression. 
 
-# Imports necessary packages and reads dataset from csv file
+# Import necessary packages
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn import tree
 from sklearn import metrics
 
+# Read dataset from csv file
 df = pd.read_csv("winequality-red.csv")
 
 # All of the 'quality' values in the dataset are between (including) 3 and 8.
@@ -25,13 +29,71 @@ quality_mapping = {
 }
 df.loc[:, "quality"] = df.quality.map(quality_mapping)
 
-# Shuffles the rows in the dataset. This is always an important step to perform
+# Shuffle the rows in the dataset. This is always an important step to perform
 # on data before training a model on that data
 df = df.sample(frac=1).reset_index(drop=True)
 
-# Splits the data into a training set (1000) and a testing/validation set (599)
+# Split the data into a training set (1000) and a testing/validation set (599)
 df_train = df.head(1000)
 df_test = df.tail(599)
 
-# Initializing a decision tree classifier with a max depth = 3
-clf = tree.DecisionTreeClassifier(max_depth=3)
+# Choose the columns that the tree classifier will be trained on,
+# which are basically the features for the model
+cols = ['fixed acidity', 
+        'volatile acidity', 
+        'citric acid', 
+        'residual sugar', 
+        'chlorides', 
+        'free sulfur dioxide', 
+        'total sulfur dioxide', 
+        'density', 
+        'pH', 
+        'sulphates', 
+        'alcohol']
+
+# Initialize lists to store the different accuracy measurements
+# from various training runs with different model depth numbers
+train_accuracies = [0.5]
+test_accuracies = [0.5]
+
+# For-loop to train the model over a few different depth values, 
+# calculating their accuracy measurements, and storing them for 
+# plotting and comparison
+for depth in range(1, 25):
+    # Initialize a decision tree classifier with depth value
+    clf = tree.DecisionTreeClassifier(max_depth=depth)
+    
+    # Train the model on the features selected and their
+    # corresponding mapped qualities
+    clf.fit(df_train[cols], df_train.quality)
+
+    # Generate predictions on the training and testing data
+    # using the trained tree classifier
+    train_predictions = clf.predict(df_train[cols])
+    test_predictions = clf.predict(df_test[cols])
+
+    # Calculcate accuracy measurements using the training and
+    # testing predictions previously generated
+    train_accuracy = metrics.accuracy_score(
+        df_train.quality, train_predictions)
+    test_accuracy = metrics.accuracy_score(
+        df_test.quality, test_predictions)
+
+    # Append the accuracy measurements into their lists
+    train_accuracies.append(train_accuracy)
+    test_accuracies.append(test_accuracy)
+
+# Set the global size of the lbel text on the plots
+matplotlib.rc('xtick', labelsize=12)
+matplotlib.rc('ytick', labelsize=12)
+
+# Create two plots using matplotlib and seaborn
+plt.figure(figsize=(10, 5))   # Set the width and height of plot in inches
+sns.set_style("whitegrid")
+plt.plot(train_accuracies, label="train accuracy")
+plt.plot(test_accuracies, label="test accuracy")
+plt.legend(loc="lower right", prop={'size' : 10})
+plt.xticks(range(0, 26, 5))
+plt.xlabel("max depth", size=12)
+plt.ylabel("accuracy", size=12)
+plt.show()
