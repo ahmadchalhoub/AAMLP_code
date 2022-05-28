6a5cfe70aa18237bcee6b5a6d26e1073d6083847
Ahmad <chalhoah@gmail.com>
2022-05-27 20:11:33 -0400

    Add stratified k-fold section

--- cross-validation/stratified_k_fold.py
+++ cross-validation/stratified_k_fold.py
@@ -14,23 +14,29 @@ from sklearn import model_selection
 if __name__ == "__main__":
 
     # Read the dataset stored in 'train.csv'
-    df = pd.read_csv("winequality-red.csv")
+    df = pd.read_csv("kfold_data_sample.csv")
 
     # Create a new column called 'kfold' and fill it with -1
     df["kfold"] = -1
 
     # Shuffle the rows of the data
     df = df.sample(frac=1).reset_index(drop=True)
+    print(df['samples'])
 
+    # Store the 'targets', or labels, of the
+    # dataset to use for stratified folding
+    y = df.target.values
+    
     # Initiate the kfold class from the 'model_selection' module
-    kf = model_selection.KFold(n_splits=5)
+    kf = model_selection.StratifiedKFold(n_splits=2)
 
-    # Fill the new kfold column and split the data into 5 sections
-    for fold, (trn_, val_) in enumerate(kf.split(X=df)):
+    # Fill the new kfold column and split the data into 2 sections
+    for fold, (trn_, val_) in enumerate(kf.split(X=df, y=y)):
         print('fold: ', fold)
         print('trn_: ', trn_)
         print('val_: ', val_)
         df.loc[val_, 'kfold'] = fold
 
-    # Save the data into a new file 'train_folds.csv'
-    df.to_csv("train_folds.csv", index=False)
+    # Save the data into a new file 'stratified_train_folds.csv'
+    df.to_csv("stratified_train_folds.csv", index=False)
+    
